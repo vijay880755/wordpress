c3de3c2a72b229c1f9ebb6a4873742f2f6c543a3
Vijay K <vijay.it.kct@gmail.com>
2021-05-23 01:34:02 +0530

    Update BuildScript.sh

--- BuildScript.sh
+++ BuildScript.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 /usr/local/bin/docker-compose -f docker-compose.yml up -d
-aws ecr get-login-password --region ap-south-1 | docker login --username AWS  --password-stdin 765771042989.dkr.ecr.ap-south-1.amazonaws.com/clever-tap
-docker tag clever-tap-wordpressdocker_wordpress 765771042989.dkr.ecr.ap-south-1.amazonaws.com/clever-tap:wordpress_$1
+docker login -u AWS -p $(aws ecr get-login-password --region ap-south-1) 765771042989.dkr.ecr.ap-south-1.amazonaws.comdocker tag clever-tap-wordpressdocker_wordpress 765771042989.dkr.ecr.ap-south-1.amazonaws.com/clever-tap:wordpress_$1
 docker tag clever-tap-wordpressdocker_wordpress 765771042989.dkr.ecr.ap-south-1.amazonaws.com/clever-tap:mysql_$1
 docker push 765771042989.dkr.ecr.ap-south-1.amazonaws.com/clever-tap:wordpress_$1
 docker push 765771042989.dkr.ecr.ap-south-1.amazonaws.com/clever-tap:mysql_$1
