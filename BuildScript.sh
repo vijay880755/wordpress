0c6739c216a20a14def51f4abf4cba0b393ab503
Vijay K <vijay.it.kct@gmail.com>
2021-05-23 01:35:26 +0530

    Correcting Typo

--- BuildScript.sh
+++ BuildScript.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 /usr/local/bin/docker-compose -f docker-compose.yml up -d
-docker login -u AWS -p $(aws ecr get-login-password --region ap-south-1) 765771042989.dkr.ecr.ap-south-1.amazonaws.comdocker tag clever-tap-wordpressdocker_wordpress 765771042989.dkr.ecr.ap-south-1.amazonaws.com/clever-tap:wordpress_$1
+docker login -u AWS -p $(aws ecr get-login-password --region ap-south-1) 765771042989.dkr.ecr.ap-south-1.amazonaws.com
+docker tag clever-tap-wordpressdocker_wordpress 765771042989.dkr.ecr.ap-south-1.amazonaws.com/clever-tap:wordpress_$1
 docker tag clever-tap-wordpressdocker_wordpress 765771042989.dkr.ecr.ap-south-1.amazonaws.com/clever-tap:mysql_$1
 docker push 765771042989.dkr.ecr.ap-south-1.amazonaws.com/clever-tap:wordpress_$1
 docker push 765771042989.dkr.ecr.ap-south-1.amazonaws.com/clever-tap:mysql_$1
