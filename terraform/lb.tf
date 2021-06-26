ddebb94a6a0c3738bfc5667caf9e879c21587c07
Vijay K <vijay.it.kct@gmail.com>
2021-06-26 19:22:17 +0530

    Update lb.tf

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -3,7 +3,7 @@ resource "aws_lb" "clevertap-wordpress" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.default.id]
-  subnets            = [aws_subnet.clevertap-subnet-1]
+  subnets            = [aws_subnet.clevertap-subnet-1.id]
 
   enable_deletion_protection = true
   tags = {
