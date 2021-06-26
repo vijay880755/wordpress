724bd38148dd4533e2bd8d895349f1230567b773
Vijay K <vijay.it.kct@gmail.com>
2021-06-26 18:35:30 +0530

    adding tg and lb rules

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -10,3 +10,37 @@ resource "aws_lb" "cleartap-wordpress" {
     Environment = "development"
   }
 }
+
+resource "aws_lb_target_group" "wordpresstg" {
+  name                               = "wordpresstg"
+  port                               = var.health_check.port
+  protocol                           = var.health_check.protocol
+  vpc_id                             = aws_vpc.clevertap-vpc
+  target_type                        = "ip"
+  proxy_protocol_v2                  = "false"
+  health_check {
+      enabled             = "true"
+      interval            = var.health_check.interval
+      port                = var.health_check.port
+      path                = var.health_check.path
+      healthy_threshold   = var.health_check.healthy_threshold
+      unhealthy_threshold = var.health_check.unhealthy_threshold
+      timeout             = var.health_check.timeout
+      protocol            = var.health_check.protocol
+      matcher             = var.health_check.matcher
+  }
+}
+
+resource "aws_alb_listener_rule" "alb_rules" {
+  listener_arn = aws_lb.cleartap-wordpress.arn
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.wordpresstg.arn
+ }
+ condition {
+    path_pattern {
+      values = ["/wp-admin/setup-config.php"]
+    }
+  } 
+ }
