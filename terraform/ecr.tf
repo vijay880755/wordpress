0292d78e3c4b517053a50adfd865bbceac3db436
Vijay K <vijay.it.kct@gmail.com>
2021-06-26 19:54:08 +0530

    Delete ecr.tf

--- terraform/ecr.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_ecr_repository" "clever-tap" {
-  name                 = "clever-tap"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = false
-  }
-}
