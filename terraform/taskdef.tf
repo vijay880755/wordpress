93887066ab0d6673163220332e1b2175adf35a24
Vijay K <vijay.it.kct@gmail.com>
2021-06-26 19:17:42 +0530

    Update taskdef.tf

--- terraform/taskdef.tf
+++ terraform/taskdef.tf
@@ -50,7 +50,7 @@ resource "aws_ecs_service" "wordpress-service" {
   }
 
   network_configuration {
-    subnets          = [aws_subnet.clevertap-subnet-1,aws_subnet.clevertap-subnet-2]
+    subnets          = [aws_subnet.clevertap-subnet-1]
     security_groups  = [ aws_security_group.default.id ]
     assign_public_ip = true
   }
