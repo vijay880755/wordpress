3d98239b86a6f7918746898299f27b1440fe0dea
Vijay K <vijay.it.kct@gmail.com>
2021-06-26 18:39:38 +0530

    Update taskdef.tf

--- terraform/taskdef.tf
+++ terraform/taskdef.tf
@@ -1,9 +1,5 @@
-data "aws_lb" "selected" {
-  name  = lookup(var.load_balancer, "name")
-}
-
 data "aws_lb_listener" "selected" {
-  load_balancer_arn = data.aws_lb.selected.arn
+  load_balancer_arn = aws_lb.clevertap-wordpress.arn
   port              = 80
 }
 
@@ -64,37 +60,3 @@ resource "aws_ecs_service" "wordpress-service" {
     container_port   = 80
   }
 }
-resource "aws_lb_target_group" "wordpresstg" {
-  name                               = "wordpresstg"
-  port                               = var.health_check.port
-  protocol                           = var.health_check.protocol
-  vpc_id                             = aws_vpc.clevertap-vpc
-  target_type                        = "ip"
-  proxy_protocol_v2                  = "false"
-  health_check {
-      enabled             = "true"
-      interval            = var.health_check.interval
-      port                = var.health_check.port
-      path                = var.health_check.path
-      healthy_threshold   = var.health_check.healthy_threshold
-      unhealthy_threshold = var.health_check.unhealthy_threshold
-      timeout             = var.health_check.timeout
-      protocol            = var.health_check.protocol
-      matcher             = var.health_check.matcher
-  }
-}
-
-resource "aws_alb_listener_rule" "alb_rules" {
-  listener_arn = data.aws_lb_listener.selected.arn
-
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.wordpresstg.arn
- }
- condition {
-    path_pattern {
-      values = ["/wp-admin/setup-config.php"]
-    }
-  } 
- }
-
