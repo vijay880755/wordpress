a9e8288e76a52395b62db320620ca81bc736928d
Vijay K <vijay.it.kct@gmail.com>
2021-06-27 17:40:36 +0530

    Update lb.tf

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -30,9 +30,17 @@ resource "aws_lb_target_group" "wordpresstg" {
       matcher             = var.health_check.matcher
   }
 }
-
+resource "aws_lb_listener" "clevertap-wordpress" {
+  load_balancer_arn = aws_lb.clevertap-wordpress.arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.wordpresstg.arn
+  }
+}
 resource "aws_alb_listener_rule" "alb_rules" {
-  listener_arn = aws_lb.clevertap-wordpress.arn
+  listener_arn = aws_lb_listener.clevertap-wordpress.arn
 
   action {
     type             = "forward"
