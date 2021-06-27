8a4633c64b0da4d7d2f55b8604517e875dabdcce
Vijay K <vijay.it.kct@gmail.com>
2021-06-27 16:40:01 +0530

    Update vpc_subnet.tf

--- terraform/vpc_subnet.tf
+++ terraform/vpc_subnet.tf
@@ -19,7 +19,7 @@ resource "aws_subnet" "clevertap-subnet-1" {
 
 resource "aws_subnet" "clevertap-subnet-2" {
     vpc_id = "${aws_vpc.clevertap-vpc.id}"
-    cidr_block = "10.0.0.0/16"
+    cidr_block = "10.0.0.0/24"
     map_public_ip_on_launch = "true" 
     availability_zone = "ap-south-1b"
     tags = {
